b45963f114570fd295fd51b4e77fd305d6b2f554
angie olave <angie.olave@outlook.com>
2023-05-17 14:44:49 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './index.css'
+import { LoginRouter } from "./router/LoginRouter";
+
+
+function App() {
+  return (
+    <div className="App">
+      <LoginRouter/>
+    </div>
+  );
+}
+
+export default App;
